f0c90fc9cc579f1caa3277afb2fa7b44f142e33e
Joshua Overkamp <jtoverka@gmail.com>
2021-08-11 19:24:54 -0400

    Updates

---
+++ NetAutoLISP/WorkingDatabaseSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace NetDBX
+{
+    /// <summary>
+    ///     Switches the current <c>HostApplicationServices.WorkingDatabase</c>
+    ///     to the supplied database.
+    /// </summary>
+    public sealed class WorkingDatabaseSwitcher : IDisposable
+    {
+        private readonly Database _previousDatabase;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="database"></param>
+        public WorkingDatabaseSwitcher(Database database)
+        {
+            this._previousDatabase = HostApplicationServices.WorkingDatabase;
+            HostApplicationServices.WorkingDatabase = database;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            HostApplicationServices.WorkingDatabase = this._previousDatabase;
+        }
+    }
+}
